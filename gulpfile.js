ada710cbd720a3213e4ff8818021e35bc8555960
Pea Lutz <pea@glocal.coop>
2016-02-12 16:17:55 -0500

    Changed requirement from gulp-minify-css to gulp-cssnano because gulp-minify-css is deprecated.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-// npm install --save-dev gulp gulp-plumber gulp-watch gulp-livereload gulp-minify-css gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
+// npm install --save-dev gulp gulp-plumber gulp-watch gulp-livereload gulp-cssnano gulp-jshint jshint-stylish gulp-uglify gulp-rename gulp-notify gulp-include gulp-sass
 
 var gulp = require('gulp'),
 	plumber = require( 'gulp-plumber' ),
 	watch = require( 'gulp-watch' ),
 	livereload = require( 'gulp-livereload' ),
-	minifycss = require( 'gulp-minify-css' ),
+	minifycss = require( 'gulp-cssnano' ),
 	jshint = require( 'gulp-jshint' ),
 	stylish = require( 'jshint-stylish' ),
 	uglify = require( 'gulp-uglify' ),
