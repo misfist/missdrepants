f60abf8fb22c9ea78e8cf9bc762cdd1808a0f678
Pea Lutz <pea@glocal.coop>
2016-02-23 19:45:29 -0500

    Separated the dequeue scripts function so it can be called with higher priority.

--- functions.php
+++ functions.php
@@ -4,30 +4,34 @@ if(! function_exists( 'missdrepants_enqueue_styles' ) ) {
 
     function missdrepants_enqueue_styles() {
 
+        $parent_style = 'ebor-style';
+        $icon_fonts = 'ebor-style';
+
+        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'missdrepants-style',
+            trailingslashit( get_stylesheet_directory_uri() ) . 'assets/styles/style.min.css',
+            array( $parent_style )
+        );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'missdrepants_enqueue_styles' );
+
+}
+
+
+if(! function_exists( 'missdrepants_dequeue_scripts' ) ) {
+
+    function missdrepants_dequeue_scripts() {
         // Don't load Simply Instagram tooltips styles
         wp_dequeue_style( 'tooltipster' );
 
         // Don't load parent theme's Google Maps script
         wp_dequeue_script( 'ebor-googlemapsapi' );
-        wp_dequeue_script( 'ebor-googlemapsapi' );
-
-        $parent_main_style = 'ebor-style';
-
-        $parent_styles = array(
-            $parent_main_style,
-            'ebor-custom',
-            'ebor-bootstrap',
-            'ebor-owl',
-            'ebor-fancybox',
-            'ebor-fancybox-thumbs'
-        );
-
-        wp_enqueue_style( $parent_main_style, get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'missdrepants-style',
-            trailingslashit( get_stylesheet_directory_uri() ) . 'assets/styles/style.min.css', $parent_styles );
+        wp_dequeue_script( 'google-maps' );
+        
     }
 
-    add_action( 'wp_enqueue_scripts', 'missdrepants_enqueue_styles' );
+    add_action( 'wp_enqueue_scripts', 'missdrepants_dequeue_scripts', 100 );
 
 }
 
